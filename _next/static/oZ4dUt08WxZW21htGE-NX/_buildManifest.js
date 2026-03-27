a357cbf888a93007f5d209c392d38e36c04eb417
wasswassim <wasswassim@gmail.com>
2026-03-28 00:39:04 +0100

    Updates

--- _next/static/oZ4dUt08WxZW21htGE-NX/_buildManifest.js
+++ _next/static/oZ4dUt08WxZW21htGE-NX/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Rankly//_next/:path+",
-        "destination": "/Rankly/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
